53fa989fb5073398043f319aecdb5e3de49797e2
Jicheol Oh <jicheol@gmail.com>
2021-05-11 16:26:31 -0700

    test

---
+++ UI.MVC/Views/Admin/AddUserToRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.MVC.Views.Admin
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tag-name")]
+    public class AddUserToRole : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
